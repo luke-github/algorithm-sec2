83bd3db6da831b666ec7ee3ecf0bd78200ca291b
Wen-Qi Cao <wenqicao@gmail.com>
2014-09-01 17:54:49 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 using namespace std;
 
-double power_handler(double x, int y){
-	double res = 1.0;
-	int power = y;
-	if(power < 0){
-		power = -y;
-		x = 1.0/x;
+string conversion(const string& str, int b1, int b2){
+	string res;
+	bool neg = str.front()=='-';
+	int num = 0;
+	for(int i = (neg?1:0);i<str.size();i++){
+		num *= b1;
+		num += isdigit(str[i])?str[i]-'0':str[i]-'A'+10;
 	}
-	while(power){
-		if(power&1){
-			res *= x;
-		}
-		x *= x;
-		power>>=1;
+	while(num){
+		int k = num % b2;
+		res.push_back(k>=10?'A'+k-10:'0'+k);
+		num /= b2;
 	}
+	if(res.empty()){
+		res.push_back(0);
+	}
+	if(neg){
+		res.push_back('-');
+	}
+	reverse(res.begin(),res.end());
 	return res;
 }
 
 int main(){
-	cout<<power_handler(2,-10);
+	cout<<conversion("1024",10,2);
 }
