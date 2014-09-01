e36a2daee4ae73a704ab84e255e9a0bc380ca2a2
Wen-Qi Cao <wenqicao@gmail.com>
2014-09-01 15:58:25 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int swap_bits(int x, int i, int j){
-	if(((x>>i)&1)!=((x>>j)&1)){
-		x ^= (1<<i)|(1<<j);
+unsigned add_algo(unsigned x, unsigned y){
+	unsigned sum = 0, k=1, temp_x=x,temp_y=y, carryin =0;
+	while(temp_x || temp_y){
+		unsigned xk=x&k, yk=y&k;
+		unsigned carryout = xk&yk || xk&carryin || yk&carryin;
+		sum |= xk^yk^carryin;
+		carryin = carryout<<1;
+		k<<=1;
+		temp_x>>=1;
+		temp_y>>=1;
 	}
-	return x;
+	return sum|carryin;
+}
+
+unsigned multiply_algo(unsigned x, unsigned y){
+	unsigned sum = 0;
+	while(x){
+		if(x&1){
+			sum = add_algo(sum,y);
+		}
+		x>>=1;
+		y<<=1;
+	}
+	return sum;
 }
 
 int main(){
-	cout<<swap_bits(4,2,0);
+	cout<<multiply_algo(4,7);
 }
