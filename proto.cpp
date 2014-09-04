d32e8f6e7e91e9d97909f89a35abbfedaf6deb78
Wen-Qi Cao <wenqicao@gmail.com>
2014-09-03 22:33:22 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int random_generator(int a, int b){
-	int t = abs(a-b)+1;
-	int res;
-	do{
-		res = 0;
-		for(int i=0;(1<<i)<t;i++){
-			res += res<<1 | zero_generator();
-		}
-	}while(res >= t);
-	return res + a;
+struct rectangle{
+	int x,y,width,height;
+};
+
+bool checker(rectangle& r, rectangle& s){
+	if(r.x<s.x+s.width && s.x<r.x+r.width && s.y<r.y+r.height && r.y<s.y+s.height){
+		return true;
+	}else{
+		return false;
+	}
 }
 
+rectangle intersection_algo(rectangle& r, rectangle& s){
+	if(checker(r,s)){
+		return {
+			max(r.x,s.x),max(r.y,s.y),min(r.x+r.width,s.x+s.width)-max(r.x,s.x),min(r.y+r.height,s.y+s.height)-max(r.y,s.y)
+		};
+	}else{
+		return {0,0,-1,-1};
+	}
+}
