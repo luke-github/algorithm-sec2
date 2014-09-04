39606032433d140b4eb476de5ca0ec98f182454c
Wen-Qi Cao <wenqicao@gmail.com>
2014-09-03 20:41:15 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
+#include <complex>
 using namespace std;
 
-int reverse(int a){
-	int res = 0;
-	while(a){
-		res = res*10 + a%10;
-		a /= 10;
+bool parlidrome_checker(int num){
+	if(num<0){
+		return false;
 	}
-	return res;
+	int num_length = floor(log10(num))+1;
+	int left_helper = pow(10,num_length-1);
+	int temp_num = num;
+	for(int i=0;i<num_length/2;i++){
+		if(num/left_helper!=temp_num%10){
+			return false;
+		}
+		num %= left_helper;
+		left_helper /=10;
+		temp_num /=10;
+	}
+	return true;
 }
 
 int main(){
-	cout<<reverse(123);
+	cout<<parlidrome_checker(123211);
 }
