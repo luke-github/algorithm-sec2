d1a86dee326cf85851800c1d52f33912790d02c6
Wen-Qi Cao <wenqicao@gmail.com>
2014-09-05 16:15:03 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -2,19 +2,11 @@
 #include <vector>
 using namespace std;
 
-bool check_farest_point(vector<int>& vec){
-	int reach = 0;
-	for(int i=0;i<vec.size();i++){
-		if(i>reach){
-			return false;
+void remove_element(vector<int>* vec, int k){
+	int index = 0;
+	for(int i=0;i<vec->size();i++){
+		if((*vec)[i]!=k){
+			(*vec)[index++]=(*vec)[i];
 		}
-		reach = max(reach,i+vec[i]);
 	}
-	return true;
-}
-
-int main(){
-	vector<int> in = {3,2,0,0,2,0,1};
-	vector<int> in2 = {3,3,1,0,2,0,1};
-	cout<<check_farest_point(in);
 }
