e2c71d9feda9a1f6aa681ecb08c997d2f252d8cb
Wen-Qi Cao <wenqicao@gmail.com>
2014-09-07 18:35:59 -0400

    init

--- 6_17.cpp
+++ 6_17.cpp
@@ -3,12 +3,19 @@
 #include <random>
 using namespace std;
 
-vector<int> sampling_algorithm(vector<int>& vec, int k){
+vector<int> random_permutation(vector<int> vec){
 	default_random_engine gen((random_device())());
-	for(int i=0;i<k;i++){
+	for(int i=0;i<vec.size();i++){
 		uniform_int_distribution<int> dis(i,vec.size()-1);
 		swap(vec[i],vec[dis(gen)]);
 	}
-	vec.resize(k);
 	return vec;
+}
+
+int main(){
+	vector<int> in = {1,2,3,4,5};
+	vector<int> res =  random_permutation(in);
+	for(int x : res){
+		cout<<x<<" ";
+	}
 }
