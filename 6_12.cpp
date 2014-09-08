e21e19a056d7bf2ef8b5741a052cb752cdbbbe1a
Wen-Qi Cao <wenqicao@gmail.com>
2014-09-08 15:22:49 -0400

    init

---
+++ 6_12.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <complex>
+using namespace std;
+
+bool checker_algorithm(vector<vector<int>>& A, int row_start, int row_end, int col_start, int col_end, int num){
+	deque<bool> is_exist(num+1,false);
+	for(int i=row_start;i<row_end;i++){
+		for(int j=col_start;j<col_end;j++){
+			if(A[i][j]!=0 && is_exist[A[i][j]]){
+				return true;
+			}
+			is_exist[A[i][j]]=true;
+		}
+	}
+	return false;
+}
+
+bool check_sudoku(vector<vector<int>>& A){
+	// check row
+	for(int i=0;i<A.size();i++){
+		if(checker_algorithm(A,i,i+1,0,A.size(),A.size())){
+			return false;
+		}
+	}
+	// check column
+	for(int i=0;i<A.size();i++){
+		if(checker_algorithm(A,0,A.size(),i,i+1,A.size())){
+			return false;
+		}
+	}
+
+	// check every rectagle
+	int unit = sqrt(A.size());
+	for(int i=0;i<unit;i++){
+		for(int j=0;j<unit;j++){
+			if(checker_algorithm(A,i*unit,(i+1)*unit,j*unit,(j+1)*unit,A.size())) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
