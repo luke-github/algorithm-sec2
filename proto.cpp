9d36c6b8583bcc0cee00a54786c321f80f936163
Wen-Qi Cao <wenqicao@gmail.com>
2014-09-10 15:59:14 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,31 +1,20 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-string remove_and_replace(string s){
-	int write_index = 0, counter_a = 0;
-	for(char c : s){
-		if(c!='b'){
-			s[write_index++] = c;
+bool check_parlidorm(string s){
+	int i=0, j=s.size()-1;
+	while(i<j){
+		while((!isalnum(s[i])) && i<j){
+			i++;
 		}
-		if(c=='a'){
-			counter_a++;
+		while((!isalnum(s[j]))&& i<j){
+			j--;
 		}
+		if(s[i]!=s[j]){
+			return false;
+		}
+		i++,j--;
 	}
-	s.resize(write_index+counter_a);
-	int cur_index = write_index - 1;
-	write_index = s.size()-1;
-	while(cur_index>=0){
-		if(s[cur_index]=='a'){
-			s[write_index--]='*';
-			s[write_index--]='*';
-		}else{
-			s[write_index--]=s[cur_index];
-		}
-		cur_index--;
-	}
-	return s;
+	return true;
 }
-int main(){
-	cout<<remove_and_replace("bbbasdfasdf");
-}
+
