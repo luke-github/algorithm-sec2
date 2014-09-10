6e64921f981ae560b2a84f680de421407f3e8acc
Wen-Qi Cao <wenqicao@gmail.com>
2014-09-10 14:49:53 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -2,21 +2,44 @@
 #include <vector>
 using namespace std;
 
-vector<vector<int>> pascal_triangle(int n){
-	vector<vector<int>> res;
-	res.emplace_back(vector<int>{1});
-	for(int i=1;i<n;i++){
-		vector<int> cur_row;
-		cur_row.emplace_back(1);
-		for(int j=1;j<i;j++){
-			cur_row.emplace_back(res.back()[j-1]+res.back()[j]);
-		}
-		cur_row.emplace_back(1);
-		res.emplace_back(cur_row);
+int stringtoint(string s){
+	bool is_negative = false;
+	int res = 0;
+	if(s[0]=='-'){
+		is_negative = true;
+	}
+	for(int i = is_negative;i<s.size();i++){
+		res = res*10;
+		res += s[i] - '0';
+	}
+	if(is_negative){
+		res = -res;
 	}
 	return res;
 }
 
+string inttostring(int x){
+	bool is_negative = false;
+	string res;
+	if(x<0){
+		x=-x;
+		is_negative = true;
+	}
+	while(x){
+		res.push_back(x%10+'0');
+		x /= 10;
+	}
+	if(res.empty()){
+		res.push_back('0');
+	}
+	if(is_negative){
+		res.push_back('-');
+	}
+	reverse(res.begin(),res.end());
+	return res;
+}
+
 int main(){
-	
+	cout<<stringtoint("123")<<endl;
+	cout<<inttostring(123)<<endl;
 }
