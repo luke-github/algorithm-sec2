b039b28158285f29c249bc17cd38831b5c145acc
Wen-Qi Cao <wenqicao@gmail.com>
2014-09-11 21:12:10 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -2,20 +2,27 @@
 #include <vector>
 using namespace std;
 
-vector<string> keypad = {"0","1","abc","edf","ghi","jkl","mno","pqrs","tuv","wxyz"};
-
-void print_combination_handler(string& num, int index, string* res){
-	if(index==num.size()){
-		cout<<*res<<endl;
-	}else{
-		for(char c : keypad[num[index]-'0']){
-			(*res)[index]=c;
-			print_combination_handler(num,index+1,res);
+string read_to_write_algorithm(string s){
+	string res;
+	for(int i=0;i<s.size();i++){
+		int count = 1;
+		while(i+1<s.size() && s[i]==s[i+1]){
+			i++;
+			count++;
 		}
+		res += to_string(count)+s[i];
 	}
+	return res;
 }
 
-void print_combination(string& num){
-	string res(num.size(),0);
-	print_combination_handler(num,0,&res);
+string read_to_write(int n){
+	string s = "1";
+	for(int i=1;i<n;i++){
+		s = read_to_write_algorithm(s);
+	}
+	return s;
+}
+
+int main(){
+	cout<<read_to_write(4);
 }
