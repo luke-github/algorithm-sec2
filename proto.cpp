6b9bf9e577f87664f1c695e5253b41b546349d72
Wen-Qi Cao <wenqicao@gmail.com>
2014-09-14 14:27:50 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-int int_to_string(string num){
-	int res = 0;
-	for(int i=0;i<num.size();i++){
-		res = res*10 + num[i]-'0';
+string tail(string filename, int line_num){
+	string res;
+	fstream file_ptr(filename);
+	file_ptr.seekg(0,ios::end);
+	int file_size = file_ptr.tellg();
+	int line_count = 0;
+	for(int i=0;i<file_size;i++){
+		file_ptr.seekg(-1-i,ios::end);
+		char c;
+		file_ptr.get(c);
+		if(c=='\n'){
+			line_count++;
+			if(line_count==line_num){
+				break;
+			}
+		}
+		res.push_back(c);
 	}
+	reverse(res.begin(),res.end());
 	return res;
 }
 
 int main(){
-	cout<<int_to_string("123");
+	string filename = "text_input";
+	cout<<tail(filename,4)<<endl;
 }
