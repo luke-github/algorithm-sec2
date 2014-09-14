3534ff956b18842834456279c3c16cd0ffac7ed2
Wen-Qi Cao <wenqicao@gmail.com>
2014-09-14 10:33:14 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,41 +1,42 @@
 #include <iostream>
+#include <sstream>
 #include <vector>
 using namespace std;
 
-bool is_valid(const string& str){
-	if((str.front()=='0'&&str.size()>1)||str.size()>3){
-		return false;
-	}
-	int val = stoi(str);
-	return val<=255&&val>=0;
-}
-
-vector<string> check_ipaddress(const string& str){
-	vector<string> res;
-	for(int i=1;i<4&&i<str.size();i++){
-		string first = str.substr(0,i);
-		if(is_valid(first)){
-			for(int j=1;j<4&&i+j<str.size();j++){
-				string second = str.substr(i,j);
-				if(is_valid(second)){
-					for(int k=1;k<4&&i+j+k<str.size();k++){
-						string thrid = str.substr(i+j,k);
-						string fouth = str.substr(i+j+k);
-						if(is_valid(thrid)&&is_valid(fouth)){
-							res.emplace_back(first+"."+second+"."+thrid+"."+fouth);
-						}
-					}
-				}
-			}
+string decode(const string& str){
+	int count = 0;
+	string res;
+	for(char c : str){
+		if(isdigit(c)){
+			count = count*10 + c- '0';
+		}else{
+			res.append(count,c);
+			count = 0;
 		}
 	}
 	return res;
 }
 
-int main(){
-	string in = "192168010";
-	auto res = check_ipaddress(in);
-	for(string x : res){
-		cout<<x<<endl;
+string encode(const string& str){
+	int count = 1;
+	stringstream res;
+	for(int i=1;i<str.size();i++){
+		if(str[i]==str[i-1]){
+			count++;
+		}else{
+			res << count << str[i-1];
+			count = 1;
+		}
 	}
+	res << count << str.back();
+	return res.str();
+}
+
+
+int main(){
+	string in = "aabbbccc";
+	cout<<encode(in)<<endl;
+	string in2 = "1a2b3c";
+	cout<<decode(in2);
+
 }
