8d3935aa7bd48715dfb35e9291c3b65a321ec662
Wen-Qi Cao <wenqicao@gmail.com>
2014-09-17 10:05:16 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 #include <memory>
 using namespace std;
 
@@ -8,69 +9,15 @@ struct ListNode{
 	shared_ptr<ListNode<T>> next;
 };
 
-shared_ptr<ListNode<int>> reverse_list(shared_ptr<ListNode<int>> head){
-	shared_ptr<ListNode<int>> cur = head, pre = nullptr;
-	while(cur){
-		shared_ptr<ListNode<int>> temp = cur->next;
-		cur->next = pre;
-		pre = cur;
-		cur = temp;
+bool check_cycle(shared_ptr<ListNode<int>> head){
+	map<shared_ptr<ListNode<int>>,bool> checker;
+	while(head){
+		if(checker[head]){
+			return true;
+		}else{
+			checker[head]=true;
+			head=head->next;
+		}
 	}
-	return pre;
-}
-
-shared_ptr<ListNode<int>> reverse_k_list(shared_ptr<ListNode<int>> head, int k){
-	shared_ptr<ListNode<int>> pre=head, before_pre=nullptr, post=head, before_post=nullptr;
-	bool first_run = true;
-	while(pre){
-		int i = k;
-		while(i){
-			i--;
-			before_post=post;
-			post=post->next;
-			if(!post){
-				break;
-			}
-		}
-		if(i){
-			return head;
-		}
-		before_post->next=nullptr;
-		reverse_list(pre);
-		if(before_pre){
-			before_pre->next=before_post;
-		}
-		if(first_run){
-			head=before_post;
-			first_run=false;
-		}
-		before_pre = pre;
-		pre->next=post;
-		pre=post;
-		before_post=nullptr;
-	}
-	return head;
-}
-
-
-
-int main(){
-	shared_ptr<ListNode<int>> n1 = make_shared<ListNode<int>>(ListNode<int>({1,nullptr}));
-	shared_ptr<ListNode<int>> n2 = make_shared<ListNode<int>>(ListNode<int>({2,nullptr}));
-	shared_ptr<ListNode<int>> n3 = make_shared<ListNode<int>>(ListNode<int>({3,nullptr}));
-	shared_ptr<ListNode<int>> n4 = make_shared<ListNode<int>>(ListNode<int>({4,nullptr}));
-	shared_ptr<ListNode<int>> n5 = make_shared<ListNode<int>>(ListNode<int>({5,nullptr}));
-	shared_ptr<ListNode<int>> n6 = make_shared<ListNode<int>>(ListNode<int>({6,nullptr}));
-	shared_ptr<ListNode<int>> n7 = make_shared<ListNode<int>>(ListNode<int>({7,nullptr}));
-	n1->next=n2;
-	n2->next=n3;
-	n3->next=n4;
-	n4->next=n5;
-	n5->next=n6;
-	n6->next=n7;
-	auto res = reverse_k_list(n1,3);
-	while(res){
-		cout<<res->data<<" ";
-		res=res->next;
-	}
+	return false;
 }
