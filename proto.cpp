7f34dc9d6dc2b8ab6c05e687b0fbbd83f67d71d5
Wen-Qi Cao <wenqicao@gmail.com>
2014-09-18 20:32:46 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,40 +1,29 @@
 #include <iostream>
-#include <memory>
+#include <stack>
 using namespace std;
 
-template<class T>
-struct ListNode{
-	T data;
-	shared_ptr<ListNode<T>> next;
-};
-
-bool handle_carry(shared_ptr<ListNode<int>>* node){
-	if((*node)->data<10){
-		return false;
+class My_Stack{
+public:
+	bool empty(){
+		return st_.empty();
 	}
-	(*node)->data -= 10;
-	if(!(*node)->next){
-		(*node)->next = make_shared<ListNode<int>>(ListNode<int>({0,nullptr}));
-	}
-	(*node)->next->data++;
-	return true;
-}
-
-shared_ptr<ListNode<int>> adder_algorithm(shared_ptr<ListNode<int>> l1, shared_ptr<ListNode<int>> l2){
-	shared_ptr<ListNode<int>> res = l1;
-	while(l2){
-		l1->data += l2->data;
-		handle_carry(&l1);
-		if(!l1->next){
-			l1->next = l2->next;
-			return res;
+	int max_element(){
+		if(empty()){
+			throw invalid_argument("invalid");
 		}
-		l1=l1->next;
-		l2=l2->next;
+		return st_.top().second;
 	}
-	while(l1 && handle_carry(&l1)){
-		l1=l1->next;
+	void push(int x){
+		st_.emplace(x,max(x,st_.empty()?x:st_.top().second));
 	}
-	return res;
-}
-
+	int pop(){
+		if(empty()){
+			throw invalid_argument("invalid");
+		}
+		int res = st_.top().first;
+		st_.pop();
+		return res;
+	}
+private:
+	stack<pair<int,int>> st_;
+};
