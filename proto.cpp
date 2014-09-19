50bc334ac1a8bfe7b58dbc319f833126cc7c72db
Wen-Qi Cao <wenqicao@gmail.com>
2014-09-18 22:15:14 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
+#include <memory>
 #include <stack>
 using namespace std;
 
-bool checker_algorithm(string str){
-	stack<char> st;
-	for(int i=0;i<str.size();i++){
-		if(str[i]=='(' || str[i]=='[' || str[i]=='{'){
-			st.emplace(str[i]);
+template<class T>
+struct BST{
+	T data;
+	shared_ptr<BST<T>> left;
+	shared_ptr<BST<T>> right;
+};
+
+void BST_stack_algorithm(shared_ptr<BST<int>> node){
+	stack<shared_ptr<BST<int>>> st;
+	shared_ptr<BST<int>> cur = node;
+	while(cur || !st.empty()){
+		if(cur){
+			st.emplace(cur);
+			cur=cur->left;
 		}else{
-			if(st.empty()){
-				return false;
-			}
-			if(str[i]=='(' && st.top()!=')'){  //there also some others
-				return false;
-			}
+			cur = st.top();
 			st.pop();
+			cout<<cur->data<<endl;
+			cur = cur ->right;
 		}
 	}
-	return st.empty();
 }
