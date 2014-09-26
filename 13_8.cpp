4f27282d55342f354a6423ba0e10b02c26fe8840
Wen-Qi Cao <wenqicao@gmail.com>
2014-09-26 17:22:26 -0400

    init

--- 13_8.cpp
+++ 13_8.cpp
@@ -3,19 +3,15 @@
 #include <unordered_map>
 using namespace std;
 
-int nearest_algorithm(vector<string> vec){
+int nearest_algorithm(vector<string>& vec){
+	int res = 1<<31 - 1;
 	unordered_map<string,int> hash;
-	int res = (1<<31) - 1;
 	for(int i=0;i<vec.size();i++){
 		auto it = hash.find(vec[i]);
 		if(it!=hash.end()){
-			res = min(res,i - hash[vec[i]]);
+			res = min(res,i - it->second);					
 		}
 		hash[vec[i]] = i;
 	}
 	return res;
-}
-
-int main(){
-	cout<<((1<<31) - 1);
 }
