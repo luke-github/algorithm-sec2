e8d16ed4cea7e3c9a2e830c48b4dffdc13685fcc
Wen-Qi Cao <wenqicao@gmail.com>
2014-09-26 14:24:41 -0400

    init

---
+++ 13_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+void check_anagram(vector<string>& dict){
+	unordered_map<string,vector<string>> hash;
+	for(string str: dict){
+		string sort_str(str);
+		sort(sort_str.begin(),sort_str.end());
+		hash[sort_str].emplace_back(str);
+	}
+	for(pair<string,vector<string>> x : hash){
+		if(x.second.size()>=2){
+			for(string s : x.second){
+				cout<<s<<endl;
+			}
+		}
+	}
+}
