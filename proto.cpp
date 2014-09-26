e5e9906213e6d61a506f942413aa6deeeed4bcf4
Wen-Qi Cao <wenqicao@gmail.com>
2014-09-25 20:29:49 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 #include <queue>
-#include <vector>
+#include <sstream>
 using namespace std;
 
-int main(){
-	priority_queue<int,vector<int>,greater<int>> max_queue;
-	max_queue.emplace(13);
-	max_queue.emplace(123);
-	max_queue.emplace(33);
-	max_queue.emplace(122);
-	max_queue.emplace(19);
-	while(!max_queue.empty()){
-		cout<<max_queue.top()<<endl;
-		max_queue.pop();
-	}
+void online_median(istringstream* sin){
+	priority_queue<int,vector<int>,greater<int>> H;
+	priority_queue<int,vector<int>,less<int>> L;
+	int x;
+	while(*sin>>x){
+		if(x>H.top()){
+			H.emplace(x);
+		}else{
+			L.emplace(x);
+		}
 
+		if(H.size()>L.size()+1){
+			L.emplace(H.top());
+			H.pop();
+		}else if(L.size()>H.size()+1){
+			H.emplace(L.top());
+			L.pop();
+		}
+
+		if(H.size()==L.size()){
+			cout<<0.5*(H.top()+L.top());
+		}else{
+			cout<< (H.size()>L.size()?H.top():L.top());
+		}
+	}
 }
