9a429d90ec5ccaca7b4a7b31787d01e8d9a20778
Wen-Qi Cao <wenqicao@gmail.com>
2014-10-14 22:25:16 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <numeric>
 using namespace std;
 
-int max_choice_handler(vector<int>& vec, int a, int b, vector<vector<int>>& dp){
-	if(a>b){
-		return 0;
+int min_parlidrom(string s){
+	vector<vector<bool>> dp(s.size(),vector<bool>(s.size(),false));
+	vector<int> res(s.size()+1);
+	iota(res.rbegin(),res.rend(),-1);
+	for(int i=s.size()-1;i>=0;i--){
+		for(int j=i;j<s.size();j++){
+			if(s[i]==s[j] && (j-i<2 || dp[i+1][j-1])){
+				dp[i][j] = true;
+				res[i] = min(res[i],1+res[j+1]);
+			}
+		}
 	}
-	if(dp[a][b]==-1){
-		dp[a][b] = max( vec[a] + min(max_choice_handler(vec,a+1,b-1,dp),max_choice_handler(vec,a+2,b,dp)),
-						vec[b] + min(max_choice_handler(vec,a+1,b-1,dp),max_choice_handler(vec,a,b-2,dp))
-							);
-	}
-	return dp[a][b];
-}
-
-int max_choice(vector<int>& vec){
-	vector<vector<int>> dp(vec.size(),vector<int>(vec.size(),-1));
-	return max_choice_handler(vec,0,vec.size()-1,dp);
+	return res[0];
 }
