bdac9b4629c28b21d856b90fda0bfa5398bb873a
Wen-Qi Cao <wenqicao@gmail.com>
2014-10-14 20:13:42 -0400

    init

--- 17_12.cpp
+++ 17_12.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
-#include <unordered_set>
 #include <vector>
+#include <unordered_set>
 using namespace std;
 
-vector<string> check_dict(unordered_set<string>& dict, string str){
+vector<string> check_makeup(unordered_set<string>& dict, string str){
 	vector<string> res;
 	vector<int> dp(str.size(),0);
 	for(int i=0;i<str.size();i++){
 		if(dict.find(str.substr(0,i+1))!=dict.end()){
-			dp[i]=i+1;
+			dp[i] = i+1;
 		}
 		for(int j=0;j<i&&dp[i]==0;j++){
-			if(dp[j]&&dict.find(str.substr(j+1,i-j))!=dict.end()){
-				dp[i] = i-j;
+			if(dp[j]&&dict.find(str.substr(j+1,i-j))!=dict.end()) {
+				dp[i]=i-j;
 			}
 		}
 	}
@@ -20,8 +20,17 @@ vector<string> check_dict(unordered_set<string>& dict, string str){
 		int index = str.size()-1;
 		while(index>=0){
 			res.emplace_back(str.substr(index - dp[index] + 1,dp[index]));
+			index -= dp[index];
 		}
 	}
 	reverse(res.begin(),res.end());
 	return res;
+}
+
+int main(){
+	unordered_set<string> in = {"abc","123","dd"};
+	auto res = check_makeup(in,"dd123abcabc");
+	for(string x : res){
+		cout<<x<<" ";
+	}
 }
