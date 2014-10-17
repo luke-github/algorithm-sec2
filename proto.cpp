db657e4879ade141477a8ae3b1f5d81fb1d711a8
Wen-Qi Cao <wenqicao@gmail.com>
2014-10-17 14:07:01 -0400

    init

--- proto.cpp
+++ proto.cpp
@@ -1,35 +1,28 @@
 #include <iostream>
-#include <unordered_map>
+#include <fstream>
 #include <vector>
+#include <unordered_map>
+#include <set>
 using namespace std;
 
-bool find_substring_handler(string str,unordered_map<string,int>& dict, int start, int unitsize,int numofword){
-	unordered_map<string,int> cur_dict;
-	for(int i=0;i<numofword;i++){
-		string cur_word = str.substr(start+i*unitsize,unitsize);
-		auto it = dict.find(cur_word);
-		if(it==dict.end()){
-			return false;
-		}
-		cur_dict[cur_word]++;
-		if(cur_dict[cur_word]>dict[cur_word]){
-			return false;
-		}
+pair<string,string> best_affinity(ifstream* sin){
+	string page;
+	string user;
+	unordered_map<string,set<string>> page_user_map;
+	while(*sin>>page>>user){
+		page_user_map[page].emplace(user);
 	}
-	return true;
-}
-
-vector<int> find_substring(vector<string>& vec, string str){
-	vector<int> res;
-	unordered_map<string, int> dict;
-	for(string s : vec){
-		dict[s]++;
-	}
-	int unitsize = vec.front().size();
-	int numofword = vec.size();
-	for(int i=0;i+unitsize*numofword<=str.size();i++){
-		if(find_substring_handler(str,dict,i,unitsize,numofword)){
-			res.emplace_back(i);
+	pair<string,string> res;
+	int max_count = 1<<31;
+	for(auto a = page_user_map.begin();a!=page_user_map.end();a++){
+		auto b =a;
+		for(advance(b,1);b!=page_user_map.end();b++){
+			vector<string> cur_res;
+			set_intersection(a->second.begin(),a->second.end(),b->second.begin(),b->second.end(),back_inserter(cur_res));
+			if(cur_res.size()>max_count){
+				max_count = cur_res.size();
+				res = {a->first,b->first};
+			}
 		}
 	}
 	return res;
